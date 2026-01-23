602597d1180faf8c4726544d42c2b8fa71c59ddf
John Wuller <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2026-01-22 21:11:19 -0500

    Remove first
    Show all the games!

--- Flashpoint.cs
+++ Flashpoint.cs
@@ -96,7 +96,7 @@ namespace Flashpoint
                     string query = "SELECT id,title,version,originalDescription,lastPlayed,playtime,playCounter,logoPath,screenshotPath FROM game WHERE id = ?";
                          
 
-                    var result = database.Query<FlashpointGame>(query, gameId).First();
+                    var result = database.Query<FlashpointGame>(query, gameId);
                     //logger.Info("ASUID");
                     //logger.Info(gameId);
                     //logger.Info(result.id);
